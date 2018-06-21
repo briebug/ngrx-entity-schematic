06d2df21975e8ec697a29acd834f6ea5878ff889
Jamie Perkins <jamie.perkins@gmail.com>
2018-06-21 16:12:35 -0600

    fix comment

--- src/app/state/entity/entity.effects.ts
+++ src/app/state/entity/entity.effects.ts
@@ -249,7 +249,7 @@ export class BriebugEffects {
       })
     );
 
-  // ========================================= PAGING
+  // ========================================= QUERY
 
   @Effect({
     dispatch: false
