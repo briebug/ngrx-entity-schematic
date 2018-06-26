02bf416d7c0eea0cd032b9e918f595f82338b2cb
kevin schuchard <schuchard.kevin@gmail.com>
2018-06-26 11:07:22 -0600

    feat(schematic): setup ngrx generator

--- src/ngrx-entity/index.ts
+++ src/ngrx-entity/index.ts
@@ -1,1 +1,35 @@
-// Schematic
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+  url,
+  apply,
+  move,
+  template,
+} from '@angular-devkit/schematics';
+import { NgRxOptions } from './utility/util';
+
+export default function(options: NgRxOptions): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    return chain([addNgRxFiles()])(tree, context);
+  };
+}
+
+function addNgRxFiles(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const dir = './'; // read path from where uses runs schematic?
+    context.logger.debug(`adding NgRX files to ${dir} dir`);
+
+    const templateSource = apply(url('./__files__'), [
+      template({
+        // ...strings,
+        // 'if-flat': (s: string) => options.flat ? '' : s,
+        // ...options,
+      }),
+      move(dir),
+    ]);
+
+    return templateSource(context);
+  };
+}
